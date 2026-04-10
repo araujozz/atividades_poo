c7a55f6c3097d7490eacd49c7e627059224deb53
araujozz <samuelgoesaraujo@gmail.com>
2026-04-10 13:33:56 -0300

    Step 3: Upgrade Java Version and Build Tools - Compile: SUCCESS, Tests: 1/1 passed

---
+++ atividade7_10_04/poo02_exemplo/poo/src/main/java/com/fatec/ads/Exame.java
@@ -0,0 +1,25 @@
+package com.fatec.ads;
+
+public class Exame extends  Prodecimento {
+    
+
+    public void solicitar(){}
+    public void consultar(){}
+
+   
+   
+    public Exame( String data, String descritivo) {
+     
+        this.data = data;
+        this.descritivo = descritivo;
+    }
+    public Exame() {
+    }
+   
+    public void mostrar() {
+      
+    }
+
+    
+    
+}
---
+++ atividade7_10_04/poo02_exemplo/poo/src/main/java/com/fatec/ads/Receita.java
@@ -0,0 +1,23 @@
+package com.fatec.ads;
+
+public class Receita extends  Prodecimento {
+   
+    public Receita(String data, String descritivo) {
+        this.data = data;
+        this.descritivo = descritivo;
+    }
+
+    public void preescrever(){};
+    public void consultar(){}
+   
+    public Receita() {
+    }
+
+   
+    public void mostrar() {
+       
+    };
+
+
+    
+}
